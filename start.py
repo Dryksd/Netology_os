83e1eeabddd6ac308d6a85763d479bc626451d98
Dryksd <samara.lawfirm@gmail.com>
2018-02-06 18:22:19 +0300

    Initial commit

---
+++ start.py
@@ -0,0 +1,17 @@
+import os
+
+def fdx():
+    strip = input('Enter your pin - code:     ')
+    def finder(strip):
+        now = os.getcwd()
+        counter = 0
+        while len(strip) != 1:
+            for
+                if os.path.exists('strip') is True:
+                    counter += 1
+        return counter
+
+print(fdx())
+
+
+
