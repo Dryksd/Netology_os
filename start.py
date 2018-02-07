ea6706a3c1e26163cb59061234e48177b8f2e3c8
Dryksd <samara.lawfirm@gmail.com>
2018-02-07 16:45:34 +0300

    new_way

--- start.py
+++ start.py
@@ -1,17 +1,20 @@
 import os
 
-def fdx():
-    strip = input('Enter your pin - code:     ')
-    def finder(strip):
-        now = os.getcwd()
-        counter = 0
-        while len(strip) != 1:
-            for
-                if os.path.exists('strip') is True:
-                    counter += 1
-        return counter
+enterous = input('Enter name file:     ')
+def finder (strip = enterous):
+    list_1 = []; counter = 0
+    current_dir = os.getcwd()
+    items_now = os.listdir(current_dir)
+    for i in items_now:
+        if i == strip:
+            list_1.append(i)
+            counter += 1
 
-print(fdx())
+    return ('Найденные файлы: {} ,Количество найденных файлов: {}'.format(list_1, counter))
+
+
+
+print(finder())
 
 
 
